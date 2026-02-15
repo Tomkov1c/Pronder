64b9fcd9bf485616f36e3ab2489fee195574315a
Tomkov1c <tomkliner9@gmail.com>
2026-02-15 22:48:48 +0100

    Implment `NewProjectPage` and other stuff

--- Pronder/Pronder/App.xaml.cs
+++ Pronder/Pronder/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -11,6 +6,15 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using Microsoft.Windows.AppLifecycle;
+using Microsoft.Windows.AppLifecycle;
+using Pronder.FileSchemes;
+using Pronder.Handlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -32,6 +36,33 @@ namespace Pronder
             _window = new MainWindow();
             _window.AppWindow.TitleBar.PreferredHeightOption = Microsoft.UI.Windowing.TitleBarHeightOption.Tall;
             _window.Activate();
+
+            var activatedArgs = Microsoft.Windows.AppLifecycle.AppInstance.GetCurrent().GetActivatedEventArgs();
+
+            if (activatedArgs.Kind == ExtendedActivationKind.File)
+            {
+                var fileArgs = activatedArgs.Data as IFileActivatedEventArgs;
+                if (fileArgs?.Files[0] is Windows.Storage.StorageFile file)
+                {
+                    var data = (SchemeV1Data)BinaryFileHandler.Read(file.Path);
+
+                    var textBlock = new TextBlock
+                    {
+                        Text = data.Name,
+                        FontSize = 24,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center
+                    };
+
+                    var fileWindow = new Window
+                    {
+                        Title = "File Opened",
+                        Content = textBlock
+                    };
+
+                    fileWindow.Activate();
+                }
+            }
         }
     }
 }
