e8270e9a6af85c219446422f17658ecc1ab95bda
Tomkov1c <tomkliner9@gmail.com>
2026-02-16 22:16:54 +0100

    Add  expand pane button hiding when the loaded page doesn't have a pane to expand

--- Pronder/Pronder/MainWindow.xaml.cs
+++ Pronder/Pronder/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using Pronder.Helpers;
 using Pronder.Interfaces;
+using Pronder.ViewModels;
 using Pronder.Views;
 using System;
 using System.Linq;
@@ -28,18 +29,19 @@ namespace Pronder
         private void OnMenuFlyoutOpening(object sender, object e)
         {
             if (sender is MenuFlyout flyout)
-            {
                 foreach (var item in flyout.Items.OfType<MenuFlyoutItem>())
-                {
                     NavigationHelper.SetFrame(item, MainWindowFrame);
-                }
-            }
         }
 
         private void OnPaneExpandButtonPressed(Microsoft.UI.Xaml.Controls.TitleBar sender, object args)
         {
             if (MainWindowFrame?.Content is INavigationViewInterface navController)
                 navController.TogglePane();
+        }
+
+        private void OnFrameNavigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            AppTitleBar.IsPaneToggleButtonVisible = MainWindowFrame.Content is INavigationViewInterface;
         }
     }
 }
